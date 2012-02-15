dec9dc14b9ab7b25dc386c1035653eacb66508b1
Ondrej Zara <ondrej.zara@gmail.com>
2012-02-15 08:58:54 +0100

    support for multiple events

--- oz.js
+++ oz.js
@@ -14,35 +14,40 @@ var OZ = {
 		add:function(elm,event,cb) {
 			var id = OZ.Event._id++;
 			var element = OZ.$(elm);
-			var fnc = cb;
-			if (element) {
-				if (element.addEventListener) {
-					element.addEventListener(event,fnc,false);
-				} else if (element.attachEvent) {
-					fnc = function() { return cb.apply(elm,arguments); }
-					element.attachEvent("on"+event,fnc);
+			var rec = [element,event,cb];
+			var parts = event.split(" ");
+			while (parts.length) {
+				var e = parts.pop();
+				if (element) {
+					if (element.addEventListener) {
+						element.addEventListener(e,cb,false);
+					} else if (element.attachEvent) {
+						element.attachEvent("on"+e,cb);
+					}
 				}
+				if (!(e in OZ.Event._byName)) { OZ.Event._byName[e] = {}; }
+				OZ.Event._byName[e][id] = rec;
 			}
-			if (!(event in OZ.Event._byName)) { OZ.Event._byName[event] = {}; }
-			var obj = OZ.Event._byName[event];
-			obj[id] = [element,event,fnc];
-			OZ.Event._byID[id] = obj;
+			OZ.Event._byID[id] = rec;
 			return id;
 		},
 		remove:function(id) {
-			var obj = OZ.Event._byID[id];
-			if (!obj) { return; }
-			var e = obj[id];
-			var elm = e[0];
-			if (elm) {
-				if (elm.removeEventListener) {
-					elm.removeEventListener(e[1],e[2],false);
-				} else if (elm.detachEvent) {
-					elm.detachEvent("on"+e[1],e[2]);
+			var rec = OZ.Event._byID[id];
+			if (!rec) { return; }
+			var elm = rec[0];
+			var parts = rec[1].split(" ");
+			while (parts.length) {
+				var e = parts.pop();
+				if (elm) {
+					if (elm.removeEventListener) {
+						elm.removeEventListener(e,rec[2],false);
+					} else if (elm.detachEvent) {
+						elm.detachEvent("on"+e,rec[2]);
+					}
 				}
+				delete OZ.Event._byName[e][id];
 			}
 			delete OZ.Event._byID[id];
-			delete obj[id];
 		},
 		stop:function(e) { e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true; },
 		prevent:function(e) { e.preventDefault ? e.preventDefault() : e.returnValue = false; },
