965ae5f72900e5372a065553b0946b4d4198a238
Ondrej Zara <ondrej.zara@gmail.com>
2012-03-27 08:44:51 +0200

    audio, touch

---
+++ touch.oz.js
@@ -0,0 +1,29 @@
+OZ.Touch = {
+	onStart: function(elm, cb) {
+		var e = ("ontouchstart" in elm ? "touchstart" : "mousedown");
+		return OZ.Event.add(elm, e, cb);
+	},
+	
+	onMove: function(elm, cb) {
+		var e = ("ontouchmove" in elm ? "touchmove" : "mousemove");
+		return OZ.Event.add(elm, e, cb);
+	},
+	
+	onEnd: function(elm, cb) {
+		var e = ("ontouchend" in elm ? "touchend" : "mouseup");
+		return OZ.Event.add(elm, e, cb);
+	},
+	
+	onActivate: function(elm, cb) {
+		var e = ("ontouchstart" in elm ? "touchstart" : "click");
+		return OZ.Event.add(elm, e, cb);
+	},
+	
+	touches: function(e) {
+		return (e.touches ? e.touches.length : 0);
+	},
+	pos: function(e) {
+		var event = (this.touches(e) ? e.touches[0] : e);
+		return [event.clientX, event.clientY];
+	}
+};
